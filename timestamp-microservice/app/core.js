11a71d52295025ab8d00def1ede3cd95fc750a50
Marcus Vinicius Monteiro de Souza <mvsouza007@gmail.com>
2016-03-29 18:08:06 -0300

    Add utils

--- timestamp-microservice/app/core.js
+++ timestamp-microservice/app/core.js
@@ -1,11 +1,7 @@
+import { isNumeric } from './utils'
+
 function isUnixTimestamp(timestamp) {
   return isNumeric(timestamp)
-}
-
-// https://github.com/jquery/jquery/blob/0c1f72667dd74bf00c6c514ebe8b7e92c3e7ad0e/src/core.js
-function isNumeric(obj) {
-  const type = typeof(obj)
-  return (type === 'number' || type === 'string') && !Number.isNaN(obj - parseFloat(obj))
 }
 
 function unixTimestampToNaturalLanguageDate(timestamp) {
