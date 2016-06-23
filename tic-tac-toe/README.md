f919abae92765c2d87de40f7c814188c81d28126
Marcus Monteiro <mvsouza007@gmail.com>
2016-06-23 09:34:49 -0300

    Clean up half-assed, non focused projects

--- tic-tac-toe/README.md
+++
@@ -1,2 +0,0 @@
-# Tic Tac Toe
-Build a Tic Tac Toe Game project from [FreeCodeCamp](https://www.freecodecamp.com/challenges/build-a-tic-tac-toe-game)
--- tic-tac-toe/config.js
+++
@@ -1,9 +0,0 @@
-import path from 'path'
-
-const __PATHS__ = {
-  app: path.join(__dirname, 'app', 'app.js'),
-  dist: path.join(__dirname, 'dist'),
-  nodeModules: path.join(__dirname, 'node_modules')
-}
-
-export { __PATHS__ }
